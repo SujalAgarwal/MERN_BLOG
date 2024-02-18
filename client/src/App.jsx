c284c527c3eb9444bfc5bda5a2e2b3e8c1a53eee
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-02-18 18:33:26 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./Pages/Home"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import About from "./Pages/About"
+import Dashboard from "./Pages/Dashboard"
+import Projects from "./Pages/Projects"
 function App() {
   return (
-    <h1 className="text-3xl text-red-300">App</h1>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/projects' element={<Projects/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
