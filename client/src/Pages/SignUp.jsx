733d7974762aa7ff2ab309cc7225ad4a261e627b
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-02-21 21:39:32 +0530

    functionality of signup page

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,9 +1,122 @@
-
-
+/* eslint-disable react/no-unescaped-entities */
+import { Link,useNavigate } from "react-router-dom";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
+import { useState} from "react";
 function SignUp() {
+  const [formdata, setformdata] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate=useNavigate();
+  const handleChange = (e) => {
+    setformdata({ ...formdata, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    if (!formdata.username || !formdata.email || !formdata.password) {
+      return setErrorMessage("Please Fill out all fields");
+    }
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formdata),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok)
+      {
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20 flex gap-5">
+      <div className="flex-1">
+        {/*left */}
+        <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-col">
+          <Link to="/" className=" font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg ">
+              Sujal's
+            </span>
+            Blog
+          </Link>
+
+          <p className="text-sm mt-5">
+            This is a demo project.You can sign up with your email and password
+            or with Google.
+          </p>
+        </div>
+        {/*right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your Username"></Label>
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Email"></Label>
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Password"></Label>
+              <TextInput
+                type="password"
+                placeholder="password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              outline
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an Account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
