f3ec5d1e7969a2a4831c8217e941b56805763878
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-02-24 11:26:14 +0530

    sign-in api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
 const errorhandler = require("../utils/error");
 
 const signup=async(req,res,next)=>{
@@ -25,5 +26,34 @@ const signup=async(req,res,next)=>{
    next(error);
   }
 }
+const signin=async(req,res,next)=>{
+   const {email,password}=req.body;
+   if(!email || !password || email=="" || password=="")
+   {
+    return next(errorhandler(400,"All fields are required"));
+   }
 
-module.exports={signup}
+   try {
+    const validuser=await User.findOne({email});
+    if(!validuser)
+    {
+      return next(errorhandler(404,"User not found"));
+    }
+    const validPassword=bcrypt.compareSync(password,validuser.password);
+    if(!validPassword)
+    {
+      return next(errorhandler(400,"Invalid Password"));
+    }
+    const token=jwt.sign({
+      id:validuser._id
+    },process.env.JWT_SECRET)
+
+    const {password:pass,...rest}=validuser._doc;
+    res.status(200).cookie('access token',token,{
+      httpOnly:true}).json(rest)
+    
+   } catch (error) {
+      next(error);
+   }
+}
+module.exports={signup,signin}
