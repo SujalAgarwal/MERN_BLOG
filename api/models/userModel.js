03008116783361393293354c48728be51cfe1d5e
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-02-25 19:27:37 +0530

    updated header with the user data

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -17,7 +17,8 @@ const user_schema = mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: "https://shorturl.at/djv12",
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
     },
   },
   { timestamps: true }
--- client/src/Components/Headers.jsx
+++ client/src/Components/Headers.jsx
@@ -1,9 +1,13 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+/* eslint-disable react/no-unescaped-entities */
+import { Avatar, Button, Dropdown,  Navbar, TextInput } from "flowbite-react";
 import { Link,useLocation} from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import {FaMoon} from "react-icons/fa"
+import {useSelector} from "react-redux"
 function Headers() {
   const path=useLocation().pathname;
+  const {currentUser}=useSelector(state=>state.user);
+  
   return (
     <Navbar className="border-b-2">
       <Link
@@ -30,19 +34,51 @@ function Headers() {
         <Button className="w-12 h-10 sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>Sign-in</Button>
-        </Link>
-        <Navbar.Toggle/>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="user"
+                img={currentUser.profilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+
+            <Dropdown.Header>
+              <Link to={"/dashboard?tab=profile"}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Dropdown.Header>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign-in
+            </Button>
+          </Link>
+        )}
+
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path==='/'} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path==='/about'} as={'div'}>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path==='/projects'} as={'div'}>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
