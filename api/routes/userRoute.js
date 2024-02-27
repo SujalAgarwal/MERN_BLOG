07d5bb7495e79b872acb9cb16989c2c24823baf8
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-02-27 19:35:43 +0530

    addded delete user api route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express=require("express");
-const { updateUser } = require("../controllers/userController");
+const { updateUser, deleteUser } = require("../controllers/userController");
 const { verifytoken } = require("../utils/verifyUser");
 const router=express.Router();
 
 //route to update the information of teh user
 router.put('/update/:userid',verifytoken,updateUser);
+router.delete('/delete/:userid',verifytoken,deleteUser)
 module.exports=router;
