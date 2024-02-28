f2a937247e1dc822ff9edc21fc24f72324771202
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-02-28 17:01:40 +0530

    created get post api

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const router=express.Router();
 const { verifytoken } = require("../utils/verifyUser");
-const create = require("../controllers/postController");
+const {create} = require("../controllers/postController");
+const {getposts} = require("../controllers/postController");
 router.post('/create',verifytoken,create);
+router.get('/getposts',getposts);
 
 module.exports=router;
